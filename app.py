58a0be8509a140d855a48e0610c19b16e4ac52be
Rishikesh <95128415+rishikeshgithu@users.noreply.github.com>
2024-01-28 11:23:45 +0530

    update1

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
-import face_recognition
-from PIL import Image, ImageDraw
+import cv2
 
-image_path_1 = "Face-1.jpg"  
-image_path_2 = "test-1.jpg"
+image_path_1 = "Face-1.jpg"
+image_path_2 = "test-3.jpg"
 
-image_1 = face_recognition.load_image_file(image_path_1)
-image_2 = face_recognition.load_image_file(image_path_2)
+image_1 = cv2.imread(image_path_1)
+image_2 = cv2.imread(image_path_2)
 
-face_locations_1 = face_recognition.face_locations(image_1)
-face_locations_2 = face_recognition.face_locations(image_2)
+gray_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2GRAY)
+gray_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2GRAY)
 
-image_1_draw = Image.fromarray(image_1)
-image_2_draw = Image.fromarray(image_2)
-draw = ImageDraw.Draw(image_1_draw)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-for face_location in face_locations_1:
-    top, right, bottom, left = face_location
-    draw.rectangle([left, top, right, bottom], outline="red")
+faces_1 = face_cascade.detectMultiScale(gray_1, scaleFactor=1.3, minNeighbors=5)
+faces_2 = face_cascade.detectMultiScale(gray_2, scaleFactor=1.3, minNeighbors=5)
 
-image_1_draw.show()
+for (x, y, w, h) in faces_1:
+    cv2.rectangle(image_1, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-face_encoding_1 = face_recognition.face_encodings(image_1, face_locations_1)[0]
-face_encoding_2 = face_recognition.face_encodings(image_2, face_locations_2)[0]
+for (x, y, w, h) in faces_2:
+    cv2.rectangle(image_2, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-results = face_recognition.compare_faces([face_encoding_1], face_encoding_2)
-if results[0]:
-    print("The faces are a match.")
+cv2.imshow('Image 1', image_1)
+cv2.imshow('Image 2', image_2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+def compare_faces(face1, face2):
+
+    return True
+
+if len(faces_1) == 1 and len(faces_2) == 1:
+    face_1 = gray_1[y:y+h, x:x+w]
+    face_2 = gray_2[y:y+h, x:x+w]
+
+    if compare_faces(face_1, face_2):
+        print("Same")
+    else:
+        print("Not Same")
 else:
-    print("The faces are not a match.")
+    print("Number of faces detected is not one in either image.")
